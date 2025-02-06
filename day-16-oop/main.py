7903a2915c5b4005417813773184c967992062fe
Eduardo Vera <this.eduardovera@gmail.com>
2025-02-06 12:42:33 -0400

    adding day 16 coffee machine opp project

---
+++ day-16-oop/main.py
@@ -0,0 +1,12 @@
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+
+
+# order = input("What would you like? (espresso/latte/cappuccino/)")
+print(coffee_maker.report())
