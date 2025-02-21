510482458b4d4c2b321024058ab0efa46fe0ace5
Eduardo Vera <this.eduardovera@gmail.com>
2025-02-20 20:11:43 -0400

    Day 24 implementing high score with file

--- day-20-snake/scoreboard.py
+++ day-20-snake/scoreboard.py
@@ -8,6 +8,9 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        # self.high_score = 0
+        with open("data.txt") as data:
+           self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(5, 270)
@@ -15,14 +18,24 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
 
-    def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(arg="GAME OVER", align=ALIGNMENT, font=FONT)
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}" , align=ALIGNMENT, font=FONT)
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write(arg="GAME OVER", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(str(self.score))
+        self.score = 0
+        self.update_scoreboard()
+
 
     def increase(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
